44f16f297a1c45970ccaf8366d7f8f0b8bb38f6a
Ramkumar M <ramkumarmani2000@gmail.com>
2023-03-31 15:27:42 +0530

    api endpoints changed in env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,10 +2,10 @@ export const environment = {
   production: true,
   API_KEY: 'dMps9XHKmv5DbdkizAjnt1BP9akA1IpJ',
   GeoPositionAPI:
-    'http://dataservice.accuweather.com/locations/v1/cities/geoposition/search?apikey=',
-  ForecastAPI: 'http://dataservice.accuweather.com/forecasts/v1/daily/5day',
+    'https://dataservice.accuweather.com/locations/v1/cities/geoposition/search?apikey=',
+  ForecastAPI: 'https://dataservice.accuweather.com/forecasts/v1/daily/5day',
   CitySearchAPI:
-    'http://dataservice.accuweather.com/locations/v1/cities/search?apikey=',
+    'https://dataservice.accuweather.com/locations/v1/cities/search?apikey=',
   WeatherApi:
-    'http://api.weatherapi.com/v1/forecast.json?key=cc1f71266946419a8ce52142201410&q=',
+    'https://api.weatherapi.com/v1/forecast.json?key=cc1f71266946419a8ce52142201410&q=',
 };
